98c7372402c782632f913e1fccc696a4aa9b30b8
Glynn <glynnm@formfill.co.uk>
2018-08-13 11:47:08 +0100

    Please help me Putro

--- js/ButtonLoad.js
+++ js/ButtonLoad.js
@@ -1,5 +1,15 @@
-$("#btn-light").on("click",function(){
-
-    $("#FireTypeBtn").css("display","block")
-
-});
+$(document).ready(() => {
+    function closeAllFire() {
+            document.getElementById("fireSTTrinkets").style.display = "none";
+            document.getElementById("fireSTTalents").style.display = "none";
+            document.getElementById("fire2TTrinkets").style.display = "none";
+            document.getElementById("fire2TTalents").style.display = "none";
+            document.getElementById("fire4TTrinkets").style.display = "none";
+            document.getElementById("fire4TTalents").style.display = "none";
+    }
+    $("#fireSTBtn").click(() => {
+    closeAllFire(); 
+            document.getElementById("fireSTTrinkets").style.display = "block";
+            document.getElementById("fireSTTrinkets").style.display = "block";
+        });
+    }
